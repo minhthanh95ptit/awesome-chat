6c3e3170a0ad2f858a7d252cb7363b185edae362
minhthanh95ptit <minhthanh95ptit@gmail.com>
2020-06-08 15:38:37 +0700

    [1.0.0] Add BabelJS

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-var express = require("express"); // nap thu vien express vao
+//var express = require("express"); // nap thu vien express vao
+import express from "express"
 var app = express();
 
 var hostname = "localhost";
 var port = 8017;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World !!!</h1>")
+  res.send("<h1>Hello World !!!</h1>")
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Hello Minh Thanh, Running at -  ${hostname}:${port}`);
+  console.log(`Hello Minh Thanh, Running at -  ${hostname}:${port}`);
 });
