5fc1cb520889189356e33e2cfe1fc238d3bae66a
minhthanh95ptit <minhthanh95ptit@gmail.com>
2020-06-09 09:38:00 +0700

    [1.0.0] Define model schemas

---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  username: String,
+  gender: { type: String, default: "male" },
+  phone: { type: Number, default: null },
+  address: { type: String, default: null },
+  avatar: { type: String, default: "avatar-default.jpg" },
+  role: { type: String, default: "user" },
+  local: {
+    email: { type: String, trim: true }, // trim: true - k ton tai khoang trong
+    password: String,
+    isActive: { type: Boolean, default: false },
+    verifyToken: String
+  }
+  facebook: {
+    uid: String,
+    token: String,
+    email: { type: String, trim: true } // trim: true - k ton tai khoang trong 
+  },
+  google: {
+    uid: String,
+    token: String,
+    email: { type: String, trim: true }
+  },
+  createdAt: { type: Number, default: Date.now },
+  updatedAt: { type: Number, default: null },
+  deletedAt: { type: Number, default: null }
+});
+
+module.exports = mongoose.model("user", userSchema) // de so it vi len CSDL auto them "s"
