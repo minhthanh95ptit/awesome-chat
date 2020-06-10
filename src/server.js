55b1f912a921e9d027cd163d54167d6bbf020513
minhthanh95ptit <minhthanh95ptit@gmail.com>
2020-06-10 22:10:25 +0700

    [1.0.0] add routes_controllers

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
 import configViewEngine from "./config/viewEngine";
-
+import initRoutes from "./routes/web";
 
 let app = express();
 
@@ -12,13 +12,8 @@ ConnectDB();
 
 configViewEngine(app);
 
-app.get("/", (req, res) => {
-  return res.render("main/master");
-});
-
-app.get("/login-register", (req, res) => {
-  return res.render("auth/loginRegister");
-})
+//init all routes
+initRoutes(app);
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
   console.log(`Hello Minh Thanh, Running at -  ${process.env.APP_HOST}:${process.env.APP_PORT}`);
